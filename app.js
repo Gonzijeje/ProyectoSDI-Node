d4a80cd6234ec727070598693692e04ec4d309d3
Gonzalo Collada Vázquez <gonzadjango@gmail.com>
2018-05-10 11:42:39 +0200

    Empezando REST

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(expressSession({
     saveUninitialized: true
 }));
 
+var jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 var crypto = require('crypto');
 
 var fileUpload = require('express-fileupload');
@@ -21,6 +24,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app,mongo);
+
+// routerUsuarioToken
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, puede ser un parámetro GET , POST o HEADER
+    var token = req.body.token || req.query.token || req.headers['token'];
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+// Aplicar routerUsuarioToken
+app.use('/api/usuarios', routerUsuarioToken);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
@@ -49,6 +87,7 @@ app.set('crypto',crypto);
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig,gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, mongo,gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapiusuarios.js")(app, gestorBD);
 
 
 app.use( function (err, req, res, next ) {
